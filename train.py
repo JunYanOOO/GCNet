0d5a3a7109f838ddbdf3c3fc99ed3387a3d361a5
JunYanOOO <h321321321ha@gmail.com>
2025-10-17 15:41:23 +0800

    1. 新增.gitignore 2. 修改train.py以在windows下使用

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 import os
 
 # Multiple gpus
-os.system("CUDA_VISIBLE_DEVICES=0,1,2,3 bash ./mmsegmentation/tools/dist_train.sh gcnet-s_4xb3-120k_cityscapes-1024x1024.py 4 --work-dir ./weight/seg")
+# os.system("CUDA_VISIBLE_DEVICES=0,1,2,3 bash ./mmsegmentation/tools/dist_train.sh gcnet-s_4xb3-120k_cityscapes-1024x1024.py 4 --work-dir ./weight/seg")
 
 # Single gpu
-# os.system("CUDA_VISIBLE_DEVICES=0 bash ./mmsegmentation/tools/dist_train.sh gcnet-s_4xb3-120k_cityscapes-1024x1024.py --work-dir ./weight/seg")
+# os.system(
+#     "CUDA_VISIBLE_DEVICES=0 bash ./mmsegmentation/tools/dist_train.sh gcnet-s_4xb3-120k_cityscapes-1024x1024.py --work-dir ./weight/seg"
+# )
+
+
+# window single gpu
+os.system(
+    "python ./mmsegmentation/tools/train.py gcnet-s_4xb3-120k_cityscapes-1024x1024.py --work-dir ./weight/seg"
+)
